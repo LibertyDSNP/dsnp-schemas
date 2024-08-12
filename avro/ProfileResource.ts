ae7fe5c2d31c609ddc31ca15add09dab55cba5d4
Wes Biggs <wes.biggs@amplica.io>
2024-08-12 15:33:33 -0500

    Fix lint

--- avro/ProfileResource.ts
+++ avro/ProfileResource.ts
@@ -1,23 +1,22 @@
 import { Schema } from "avsc";
 
 const ProfileResource: Schema = {
-  "namespace": "org.dsnp",
-  "name": "ProfileResource",
-  "type": "record",
-  "doc": "A relationship to another DSNP user",
-  "fields": [
+  namespace: "org.dsnp",
+  name: "ProfileResource",
+  type: "record",
+  doc: "A relationship to another DSNP user",
+  fields: [
     {
-      "name": "type",
-      "type": "int",
-      "doc": "Type of resource"
+      name: "type",
+      type: "int",
+      doc: "Type of resource",
     },
     {
-      "name": "contentAddress",
-      "type": "string",
-      "doc": "Content address for the resource"
-    }
-  ]
+      name: "contentAddress",
+      type: "string",
+      doc: "Content address for the resource",
+    },
+  ],
 };
 
 export default ProfileResource;
-
