267dc1c3a5787a1122d83a276500d2b3674b90c0
Joe Caputo <joseph.caputo@projectliberty.io>
2025-08-01 09:37:04 -0400

    feat: add support for optional fields

--- parquet.spec.ts
+++ parquet.spec.ts
@@ -46,3 +46,67 @@ describe("DSNP Schema Conversion Test File", () => {
     expect(row).toEqual(row1);
   });
 });
+
+describe("Schema Conversion with optional fields", () => {
+  const [parquetSchema, writerOptions] = fromDSNPSchema([
+    { name: "requiredColumnOne", column_type: "INT32", compression: "UNCOMPRESSED", bloom_filter: false },
+    {
+      name: "optionalColumnTwo",
+      column_type: "INT32",
+      compression: "UNCOMPRESSED",
+      bloom_filter: false,
+      optional: true,
+    },
+    { name: "requiredColumnThree", column_type: "INT32", compression: "UNCOMPRESSED", bloom_filter: false },
+    {
+      name: "optionalColumnFour",
+      column_type: "INT32",
+      compression: "UNCOMPRESSED",
+      bloom_filter: false,
+      optional: true,
+    },
+  ]);
+
+  const rows = [
+    { requiredColumnOne: 1, requiredColumnThree: 2 },
+    { requiredColumnOne: 3, requiredColumnThree: 4, optionalColumnTwo: 5 },
+    { requiredColumnOne: 6, requiredColumnThree: 7, optionalColumnFour: 8 },
+    { requiredColumnOne: 9, optionalColumnTwo: 10, requiredColumnThree: 11, optionalColumnFour: 12 },
+  ];
+
+  const path = "./optional-column-test-file.parquet";
+
+  let reader: ParquetReader;
+
+  beforeAll(async () => {
+    const writer = await ParquetWriter.openFile(new ParquetSchema(parquetSchema), path, writerOptions);
+    await Promise.all(rows.map((row) => writer.appendRow(row)));
+    await writer.close();
+
+    reader = await ParquetReader.openFile(path);
+  });
+
+  afterAll(async () => {
+    await reader.close();
+    fs.rmSync(path);
+  });
+
+  it("schema is generated correctly", () => {
+    expect(new ParquetSchema(parquetSchema)).toMatchSnapshot();
+  });
+
+  it("schema is encoded correctly", () => {
+    expect(reader.metadata?.schema).toMatchSnapshot();
+  });
+
+  it("output matches input with null columns added", async () => {
+    const cursor = reader.getCursor();
+    let row: unknown;
+    let i = 0;
+    while ((row = await cursor.next())) {
+      const rowWithNulls = { optionalColumnTwo: null, optionalColumnFour: null, ...rows[i] };
+      expect(row).toEqual(rowWithNulls);
+      i++;
+    }
+  });
+});
